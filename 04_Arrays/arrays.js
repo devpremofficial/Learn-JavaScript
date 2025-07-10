ba9acf55c22bf0a87f55d7751f7409c3020f9b27
Prem chand <official.premdev@gmail.com>
2025-07-10 08:35:48 +0530

    initial commit

---
+++ 04_Arrays/arrays.js
@@ -0,0 +1,25 @@
+// Array creation
+
+let cars = ["Thar", "BMW", "Scorpio"];
+// console.log(cars);
+
+// Array methods
+// Push method
+cars.push("Toyota");
+cars.push("Ferrari");
+console.log("Push",cars);
+
+// Pop method
+cars.pop();
+console.log("Pop",cars);
+console.log('-----------------------------------------');
+
+
+// Unshift method
+cars.unshift("Ferrari")
+console.log("Unshift",cars);
+
+
+// Shift method
+cars.shift();
+console.log("Shift",cars);
