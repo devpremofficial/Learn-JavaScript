24d2519d556495cbcfb54fbed72dc47c889df6a1
Prem chand <official.premdev@gmail.com>
2025-07-13 10:26:19 +0530

    initial commit

---
+++ 06_Loops/loops.js
@@ -0,0 +1,16 @@
+// for (let i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+
+// Printing odd numbers from 1 to 15
+
+for (let i=1; i<=15; i=i+2){
+    // console.log(i);
+}
+
+
+// Printing even numbers from 1 to 15
+for(let i=2; i<=15; i=i+2){
+    console.log(i);
+}
