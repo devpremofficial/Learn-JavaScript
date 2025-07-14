7dbec1588fb73bd8da6c00b09bd7c8146ca46074
Prem chand <official.premdev@gmail.com>
2025-07-14 09:02:13 +0530

    initial commit

---
+++ 07_Object Literals/object.js
@@ -0,0 +1,23 @@
+const student = {
+    name: "Prem chand",
+    age: 20,
+    course: "BCA",
+    city: "Begusarai"
+};
+
+// console.log(student);
+
+const post = {
+    username: "@devpremofficial",
+    content : "This is my first post on X",
+    likes: 268,
+    reposts: 60,
+    tags: ["@ApnaCollegeX", "@sharadhakhapra"]
+};
+
+// console.log(post);
+
+//* GET VALUES
+
+console.log(post["tags"]);
+console.log(post.tags);
