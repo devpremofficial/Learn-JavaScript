91ddbd177db54a2c0b2616dee44a773c3734869c
Prem chand <official.premdev@gmail.com>
2025-07-30 06:48:01 +0530

    initial commit

---
+++ 14_DOM/14.7_Form Events/app.js
@@ -0,0 +1,13 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let user = document.querySelector(".user");
+    let pass = document.querySelector(".pass");
+
+    // console.log(user.value);
+    // console.log(pass.value);
+    alert(`Hi ${user.value}. Your password is set to ${pass.value}`);
+
+})
