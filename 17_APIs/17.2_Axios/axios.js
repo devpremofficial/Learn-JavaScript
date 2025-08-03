850265c308bb2f750aa24830f0629d6145565f1b
Prem chand <official.premdev@gmail.com>
2025-08-03 07:39:40 +0530

    initial commit

---
+++ 17_APIs/17.2_Axios/axios.js
@@ -0,0 +1,12 @@
+const url = "https://catfact.ninja/fact";
+
+async function getFact() {
+    try{
+        let res = await axios.get(url);
+        console.log(res.data.fact);
+    } catch(err){
+        console.log(err);
+    }
+} 
+
+getFact();
